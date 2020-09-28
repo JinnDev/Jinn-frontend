6f0ef300165bd5842daef7557989c159a405d602
Finn Bauer <finn.bauer18@imperial.ac.uk>
2020-09-28 19:57:52 +0200

    Full fledged

--- src/components/account/index.js
+++ src/components/account/index.js
@@ -1,12 +1,19 @@
 import React from 'react';
+import { AuthUserContext, withAuthorization } from '../session';
 import { PasswordForgetForm } from '../passwordForget';
 import PasswordChangeForm from '../passwordChange';
 
 const AccountPage = () => (
-  <div>
-    <h1>Account Page</h1>
-    <PasswordForgetForm />
-    <PasswordChangeForm />
-  </div>
+  <AuthUserContext.Consumer>
+    {authUser => (
+      <div>
+        <h1>Account: {authUser.email}</h1>
+        <PasswordForgetForm />
+        <PasswordChangeForm />
+      </div>
+    )}
+  </AuthUserContext.Consumer>
 );
-export default AccountPage;
+
+const condition = authUser => !!authUser;
+export default withAuthorization(condition)(AccountPage);

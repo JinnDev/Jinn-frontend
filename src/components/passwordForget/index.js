2a80e63963e9564e34c8ad32ce5ed1b4b8adbd72
Finn Bauer <finn.bauer18@imperial.ac.uk>
2020-11-03 19:03:32 +0100

    Style password forget

--- src/components/passwordForget/index.js
+++ src/components/passwordForget/index.js
@@ -2,11 +2,21 @@ import React, { Component } from 'react';
 import { Link } from 'react-router-dom';
 import { withFirebase } from '../firebase';
 import * as ROUTES from '../constants/routes';
+import { Layout, Row, Form, Input, Icon, Button, Col, message } from 'antd';
+import {
+  MailOutlined
+} from '@ant-design/icons';
+import './index.css';
 
 const PasswordForgetPage = () => (
-  <div>
-    <h1>PasswordForget</h1>
-    <PasswordForgetForm />
+  <div style={{ background: '#fff', padding: 28, minHeight: '70vh'}}>
+    <Row justify="center" type="flex">
+      <h2>Password Forget</h2>
+    </Row>
+    <br/>
+    <Row justify="center" type="flex">
+      <PasswordForgetForm />
+    </Row>
   </div>
 );
 
@@ -42,19 +52,25 @@ class PasswordForgetFormBase extends Component {
     const { email, error } = this.state;
     const isInvalid = email === '';
     return (
-      <form onSubmit={this.onSubmit}>
-        <input
+      <Form onSubmit={this.onSubmit} className="password-forget-form">
+        <Input
+          prefix={<MailOutlined />}
           name="email"
           value={this.state.email}
           onChange={this.onChange}
           type="text"
-          placeholder="Email Address"
+          placeholder="Enter Email Address"
+          allowClear={true}
         />
-        <button disabled={isInvalid} type="submit">
+        <br/>
+        <br/>
+        <Button disabled={isInvalid} onClick={this.onSubmit} type="primary" htmlType="submit" className="password-forget-form-button">
           Reset My Password
-        </button>
+        </Button>
+        <br/>
+        <br/>
         {error && <p>{error.message}</p>}
-      </form>
+      </Form>
     );
   }
 }
